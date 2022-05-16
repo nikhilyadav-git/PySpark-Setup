c43a7b6c6fd7f1837218225468189c8de370b733
@nikhil-yadav <88660182+nikhilyadav-git@users.noreply.github.com>
2022-05-16 02:21:55 +0100

    updated generic names in S3_ec2_mounting.sh
    replaced actual names with generic names

--- S3_ec2_mounting.sh
+++ S3_ec2_mounting.sh
@@ -19,20 +19,20 @@
 
 #Now create a directory or provide the path of an existing directory and mount S3bucket in it.
 > mkdir /nemis-datalake
->  sudo s3fs s3-kafka-nemis /nemis-datalake -o iam_role="EC2-BoomiPython-Amazon2-P-DellBoomiETLPythonServer-1NJ8TT6QVQTO8" -o url="https://s3-eu-west-1.amazonaws.com" -o endpoint=eu-west-1 -o dbglevel=info -o curldbg
+>  sudo s3fs s3-bucketName /s3-mountFolderName -o iam_role="EC2_S3FullAccessIAMRoleName" -o url="https://s3-eu-west-1.amazonaws.com" -o endpoint=eu-west-1 -o dbglevel=info -o curldbg
 
 #You can make an entry in /etc/rc.local to automatically remount after reboot.  Find the s3fs binary file by “which” command and make the entry before the “exit 0” line as below.
 > which s3fs
 #/bin/s3fs
 > nano /etc/rc.local
 #####Add below line#####
-/bin/s3fs s3-kafka-nemis -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 /nemis-datalake
+/bin/s3fs s3-bucketName -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 /nemis-datalake
 ########################
 
 #Check mounted s3 bucket. Output will be similar as shown below but Used size may differ.
-> df -Th /nemis-datalake/
+> df -Th /s3-mountFolderName/
 
 #If it shows the mounted file system, you have successfully mounted the S3 bucket on your EC2 Instance. You can also test it further by creating a test file.
-> cd /nemis-datalake
+> cd /s3-mountFolderName
 > echo "this is a test file to check s3fs" >> test.txt
 > ls
